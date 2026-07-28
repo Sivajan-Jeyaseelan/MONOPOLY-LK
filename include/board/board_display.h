fef01feb53be2a141829bbc6bb9aa5c0925be382
Sivajan-Jeyaseelan <jeyaseelansivajan@gmail.com>
2026-07-29 01:06:51 +0530

    refactor: split board module headers into separate components

---
+++ include/board/board_display.h
@@ -0,0 +1,8 @@
+#ifndef BOARD_DISPLAY_H
+#define BOARD_DISPLAY_H
+
+#include "../types.h"
+
+void displayBoard(Board board[]);
+
+#endif
---
+++ include/board/board_initializer.h
@@ -0,0 +1,8 @@
+#ifndef BOARD_INITIALIZER_H
+#define BOARD_INITIALIZER_H
+
+#include "../types.h"
+
+void initializeBoard(Board board[]);
+
+#endif
